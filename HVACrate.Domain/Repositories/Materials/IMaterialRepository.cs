2d5f388507a8f36c8b8f7237a0b5289f146e83b9
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2025-07-22 12:27:29 +0300

    Added material repository

---
+++ HVACrate.Domain/Repositories/Materials/IMaterialRepository.cs
@@ -0,0 +1,8 @@
+﻿using HVACrate.Domain.Entities;
+
+namespace HVACrate.Domain.Repositories.Materials
+{
+    public interface IMaterialRepository : IBaseRepository<Material>
+    {
+    }
+}
---
+++ HVACrate.Persistence/Repositories/Materials/MaterialRepository.cs
@@ -0,0 +1,8 @@
+﻿using HVACrate.Domain.Repositories.Materials;
+
+namespace HVACrate.Persistence.Repositories.Materials
+{
+    public class MaterialRepository(HVACrateDbContext context) : BaseRepository<Material>(context), IMaterialRepository
+    {
+    }
+}
